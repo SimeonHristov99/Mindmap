82873ec426ed28d07ec0e5f88a6fcc4d515b734a
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-06-14 15:29:12 +0300

    Bug fix.
    When the user entered a title for a document that was too short the API crashed.

--- frontend/src/app/pages/new-document/new-document.component.ts
+++ frontend/src/app/pages/new-document/new-document.component.ts
@@ -12,13 +12,18 @@ import { Shape } from 'src/app/models/shape.model';
 })
 export class NewDocumentComponent implements OnDestroy {
 
-  private subscription: Subscription = new Subscription();
+  private subscription = new Subscription();
+  private isTooShort = false;
 
   constructor(
     private docServ: DocumentService,
     private router: Router,
   ) { }
 
+  /**
+   * This method is a lifecycle hook in which
+   * any subscriptions are cleared.
+   */
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
@@ -30,9 +35,26 @@ export class NewDocumentComponent implements OnDestroy {
    *     The name of the document to be created.
    */
   createDocument(title: string): void {
+    if (title.length < 3) {
+      this.isTooShort = true;
+      return;
+    }
+
     this.subscription = this.docServ.createDocument(title).subscribe((document: object) => {
       this.router.navigate(['/docs', (document as Document)._id]);
     });
+  }
+
+  /**
+   * This method returns whether or not the title is too short
+   * for the document to be created.
+   * 
+   * @returns
+   *     An indication of whether or not the title is too short
+   *     for the document to be created.
+   */
+  titleTooShort(): boolean {
+    return this.isTooShort;
   }
 
 }
